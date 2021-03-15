d8fab60a8521780e691ff9ea1340167f27d23e26
omar-etman <omartareketman@gmail.com>
2021-03-15 05:04:05 +0200

    modify server side code

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,8 @@
-let projectData = {};
+let projectData = {
+  lat: '', 
+  lon: '',
+  city: '',
+};
 
 const express = require('express');
 
@@ -17,21 +21,8 @@ app.use(express.static('website'));
 const port = 8000;
 const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)});
 
-app.get('/all', sendData);
-
-function sendData (req, res) {
-  res.send(projectData);
-};
-
-app.post('/add', callBack);
+app.post('/getWeather', callBack);
 
 function callBack(req,res){
   res.send('POST received');
-};
-
-app.post('/user/input', addUserEntry);
-
-function addUserEntry (req,res){
-    projectData = Object.assign({},req.body);
-    res.send('success');
 };
