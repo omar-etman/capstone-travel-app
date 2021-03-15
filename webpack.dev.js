43e479bc708d13d7aa55005e826a976a14456df4
omar-etman <omartareketman@gmail.com>
2021-03-15 06:19:05 +0200

    Apply service workers

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-// const WorkboxPlugin = require('workbox-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: ['@babel/polyfill', './src/client/index.js'],
@@ -40,7 +40,7 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
-        // new WorkboxPlugin.GenerateSW(),
+        }),
+        new WorkboxPlugin.GenerateSW(),
     ]
 }
